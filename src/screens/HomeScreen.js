37403e5fe27217fe8a0f36ae8575a486b431d6be
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-03-15 11:57:49 +0000

    removed button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Row, Col, Image } from 'react-bootstrap';
+import { Row, Col, Image } from 'react-bootstrap';
 import projects from '../projects';
 import Project from '../components/Project';
 
