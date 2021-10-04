d8b237bbd45bade3560a235e382eca3fbd33fd29
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-10-04 11:52:46 +0100

    card title moved to top

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Card } from 'react-bootstrap';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
 
 const Project = ({ project }) => {
-	return (
-		<Card className="projects-list">
-			<Card.Body>
-				<Link to={`/project/${project._id}`}>
-					<Card.Img src={project.image} variant="top" />
-				</Link>
-				<Link to={`/project/${project._id}`}>
-					<Card.Title as="div">{project.name}</Card.Title>
-				</Link>
-			</Card.Body>
-		</Card>
-	);
+  return (
+    <Card className="projects-list">
+      <Card.Body>
+        <Card.Title as="div">{project.name}</Card.Title>
+        <Link to={`/project/${project._id}`}>
+          <Card.Img src={project.image} variant="top" />
+        </Link>
+        <Link to={`/project/${project._id}`}></Link>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Project;
